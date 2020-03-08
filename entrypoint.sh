6ed46bcbb032005fb49bff98fc84aadbea3781c0
Amim Knabben <amim.knabben@gmail.com>
2020-03-08 09:24:31 -0400

    Copying file to workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -l
 
-WORKDIR=${PWD}
+WD=/app
 
 if [ ! -d /github/workflow ]; then
    mkdir -p /github/workflow
 fi
 
-${WORKDIR}/gql scrape --url https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex
-cp ${WORKDIR}/sqlite3 /github/workflow/
+${WD}/gql scrape --url https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex
+cp ${WD}/sqlite3 /github/workspace/
+
 echo ::set-output name=file::sqlite3
