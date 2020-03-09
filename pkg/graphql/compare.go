802872a3896020ec591bea84e45e8ad07b738983
Amim Knabben <amim.knabben@gmail.com>
2020-03-09 11:19:07 -0400

    Adding compare funcs

--- pkg/graphql/compare.go
+++ pkg/graphql/compare.go
@@ -1,2 +1,50 @@
 package graphql
 
+import (
+	"github.com/knabben/ggql/ent"
+	"reflect"
+)
+
+var (
+	REMOVED_FIELD = "REMOVED_FIELD"
+	ADDED_FIELD   = "ADDED_FIELD"
+)
+
+type FieldError struct {
+	Field   ent.FieldType
+	Message string
+}
+
+func compareObjectType(source, destination ent.ObjectType) bool {
+	return reflect.DeepEqual(source, destination)
+}
+
+func hasElement(source ent.FieldType, fieldtype []ent.FieldType) bool {
+	for _, field := range fieldtype {
+		if reflect.DeepEqual(source, field) {
+			return true
+		}
+	}
+	return false
+}
+
+func compareFieldType(source, destination []ent.FieldType) ([]FieldError, []FieldError) {
+	var (
+		removed = []FieldError{}
+		added  = []FieldError{}
+	)
+
+	for _, sourceField := range source {
+		if !hasElement(sourceField, destination) {
+			added = append(added, FieldError{Field: sourceField, Message: ADDED_FIELD})
+		}
+	}
+
+	for _, destField := range destination {
+		if !hasElement(destField, source) {
+			removed = append(removed, FieldError{Field: destField, Message: REMOVED_FIELD})
+		}
+	}
+
+	return added, removed
+}
--- pkg/graphql/compare_test.go
+++ pkg/graphql/compare_test.go
@@ -1,14 +1,63 @@
 package graphql
 
 import (
+	"github.com/knabben/ggql/ent"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+func TestGraphqlCompareObjectType(t *testing.T) {
+	// Test the rootQuery name change.
 
-func TestGraphqlCompare(t *testing.T) {
-	var a string = "Hello"
-	var b string = "Hello"
+	var tests = []struct {
+		source      ent.ObjectType
+		destination ent.ObjectType
+		result      bool
+	}{
+		{ent.ObjectType{}, ent.ObjectType{}, true},
+		{ent.ObjectType{Name: "Query"}, ent.ObjectType{}, false},
+		{ent.ObjectType{Name: "Query", Description: "description"}, ent.ObjectType{Name: "RootQuery", Description: ""}, false},
+	}
 
-	assert.Equal(t, a, b, "The two words should be the same.")
+	for _, test := range tests {
+		assert.Equal(t, test.result, compareObjectType(test.source, test.destination))
+	}
+}
+
+func TestGraphqlCompareFields(t *testing.T) {
+
+	var tests = []struct {
+		source      []ent.FieldType
+		destination []ent.FieldType
+		added       []FieldError
+		removed     []FieldError
+	}{
+		{
+			[]ent.FieldType{},
+			[]ent.FieldType{},
+			[]FieldError{},
+			[]FieldError{},
+		},
+		{
+			[]ent.FieldType{{Name: "name1"}, {Name: "name2"}},
+			[]ent.FieldType{{Name: "name2"}},
+			[]FieldError{{Field: ent.FieldType{Name: "name1"}, Message: ADDED_FIELD}},
+			[]FieldError{},
+		}, // Added field
+		{
+			[]ent.FieldType{{Name: "name1"}, {Name: "name2"}},
+			[]ent.FieldType{{Name: "name3"}},
+			[]FieldError{
+				{Field: ent.FieldType{Name: "name1"}, Message: ADDED_FIELD},
+				{Field: ent.FieldType{Name: "name2"}, Message: ADDED_FIELD},
+			},
+			[]FieldError{{Field: ent.FieldType{Name: "name3"}, Message: REMOVED_FIELD}},
+		},
+	}
+
+	for _, test := range tests {
+		added, removed := compareFieldType(test.source, test.destination)
+		assert.Equal(t, test.added, added)
+		assert.Equal(t, test.removed, removed)
+	}
 }
