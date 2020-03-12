970cc73bf6eb3bd6905c6ab7e11deee3f1b55ea2
Amim Knabben <amim.knabben@gmail.com>
2020-03-12 10:35:56 -0400

    Parsing module

--- cmd/diff.go
+++ cmd/diff.go
@@ -1,21 +1,109 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+
+	c "github.com/knabben/ggql/pkg/client"
+	"github.com/knabben/ggql/pkg/database"
+	"github.com/knabben/ggql/pkg/graphql"
 	"github.com/spf13/cobra"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	sqlite, file, url string
+	result            graphql.Schema
+	ctx               = context.Background()
+	variables         = map[string]interface{}{}
 )
 
 func init() {
-	rootCmd.AddCommand(diffCmd)
+	scrapeCmd.Flags().StringVarP(&url, "url", "u", "", "GraphQL URI")
+	scrapeCmd.Flags().StringVarP(&file, "file", "f", "", "Schema file")
+	scrapeCmd.Flags().StringVarP(&sqlite, "sqlite", "s", "sqlite3", "SQLite3 database")
+
+	rootCmd.AddCommand(scrapeCmd)
 }
 
-var diffCmd = &cobra.Command{
-	Use: "diff",
-	Short: "diff",
+var scrapeCmd = &cobra.Command{
+	Use:   "scrape",
+	Short: "scrape",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Diff")
+		var uri = fmt.Sprintf("file:%s?_fk=1", sqlite)
 
+		loadResult()
 
+		database := database.NewDatabase(uri)
+		client, err := database.Connect()
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+
+		defer client.Close()
+
+		if err := client.Schema.Create(ctx); err != nil {
+			log.Fatalf("failed creating schema resources: %v", err)
+		}
+		_, err = database.CreateObjectType(ctx, result)
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
 	},
 }
 
+func loadResult() {
+	if _, err := os.Stat(sqlite); os.IsExist(err) {
+		log.Fatalf("A database already exists.")
+	}
+
+	// Hit the GraphQL endpoint.
+	if url != "" {
+		parseGraphQLRequest()
+
+	}
+
+	// Use file for schema parser
+	if file != "" {
+		parseJSONSchema()
+	}
+
+	if result.Schema.QueryType.Name == "" {
+		log.Fatalf("Error parsing the schema.")
+	}
+}
+
+// parseGraphQLRequest GraphQL endpoint hit and result dump.
+func parseGraphQLRequest() {
+
+	err := c.NewClient(url).GraphQL(graphql.BuildIntrospectionQuery(), variables, &result)
+	if err != nil {
+		log.Fatalf("Trying to fetch GraphQL endpoint %s", err)
+	}
+
+	if result.Schema.QueryType.Name == "" {
+		log.Fatalf("Error trying to fetch schema.")
+	}
+}
+
+// parseJSONSchema parsing the file and dumps the result.
+func parseJSONSchema() error {
+	schema, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		log.Fatalf("Trying to read file: %v", err)
+	}
+
+	gr := &c.GraphQLResponse{Data: &result}
+	err = json.Unmarshal(schema, &gr)
+	if err != nil {
+		log.Fatalf("Trying to unmarshal result: %v", err)
+	}
+
+	return nil
+}
---
+++ pkg/parser/parse.go
@@ -0,0 +1,76 @@
+package parser
+
+import (
+	c "github.com/knabben/ggql/pkg/client"
+
+	"encoding/json"
+	"github.com/knabben/ggql/pkg/graphql"
+	"io/ioutil"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Parse struct {
+	schemaFile, schemaUrl string
+}
+
+func NewParser(file, url string) *Parse {
+	return &Parse{
+		schemaFile: file,
+		schemaUrl:  url,
+	}
+}
+
+func (p *Parse) LoadResult() (graphql.Schema, error) {
+	// Hit the GraphQL endpoint.
+	if p.schemaUrl != "" {
+		return p.parseURLSchema()
+	}
+
+	// Use file for schema parser
+	if p.schemaFile != "" {
+		return p.parseFileSchema()
+	}
+
+	return graphql.Schema{}, nil
+}
+
+// parseGraphQLRequest Hits an endpoint and dumps the result
+func (p *Parse) parseURLSchema() (graphql.Schema, error) {
+	var (
+		variables = map[string]interface{}{}
+		result    = graphql.Schema{}
+	)
+
+	err := c.NewClient(p.schemaUrl).GraphQL(graphql.BuildIntrospectionQuery(), variables, &result)
+	if err != nil {
+		return result, err
+	}
+
+	if result.Schema.QueryType.Name == "" {
+		return result, nil
+	}
+
+	return result, nil
+}
+
+// parseFileSchema parses a schema file and dumps the result.
+func (p *Parse) parseFileSchema() (graphql.Schema, error) {
+	var (
+		result = graphql.Schema{}
+	)
+
+	schema, err := ioutil.ReadFile(p.schemaFile)
+	if err != nil {
+		return result, err
+	}
+
+	gr := &c.GraphQLResponse{Data: &result}
+
+	err = json.Unmarshal(schema, &gr)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
